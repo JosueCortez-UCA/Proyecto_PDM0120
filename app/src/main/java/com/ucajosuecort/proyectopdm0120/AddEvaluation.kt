70e1ec98eecb317fb5b557f7174cd0eafb903e16
JosueCortez-UCA <00402918@uca.edu.sv>
2020-05-28 19:14:15 -0600

    ADD: pantallas base de la app

---
+++ app/src/main/java/com/ucajosuecort/proyectopdm0120/AddEvaluation.kt
@@ -0,0 +1,12 @@
+package com.ucajosuecort.proyectopdm0120
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEvaluation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_evaluation)
+    }
+}
---
+++ app/src/main/java/com/ucajosuecort/proyectopdm0120/sing_in.kt
@@ -0,0 +1,12 @@
+package com.ucajosuecort.proyectopdm0120
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sing_in : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_in)
+    }
+}
